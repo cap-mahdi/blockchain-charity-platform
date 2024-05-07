fa43334b6919f98477863cf76b1dcfc257c9176f
Mahdi Chaabane <mahdi02ch@gmail.com>
2024-05-07 21:14:39 +0100

    Create Footer Component

--- apps/ui/src/pages/home/index.tsx
+++ apps/ui/src/pages/home/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { ExpiringSoon } from './components/ExpiringSoon';
 import { AvailableCompain } from './components/AvailableCompain';
-import { FullBleedCarousel } from '../../components/carousel/FullBleedCarousel';
 import { StyledChart } from '../../components/chart/StyledChart';
 import { Card } from '../../components/Card';
 
-export function HomePage(props) {
+export function HomePage() {
   return (
     <div className="flex flex-col gap-4 w-[100%] justify-center items-center">
       <Card className="h-[25rem] p-8">
